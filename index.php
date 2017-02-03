5de6ec563a0e2e310d47cde5db8c29152daa0c93
harismunawar09 <harismunawar09@gmail.com>
2017-02-03 21:32:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<body>
+<?php
+echo "Belajar PHP Untuk Tugas Pemograman WEB II";
+$nama="Haris Munawar";
+$nim="14 111 126";
+$kelas="TIF 14 RM";
+echo "<p>Nama: $nama</p>";
+echo "<p>Nim: $nim</p>";
+echo "<p>Kelas: $kelas</p>";
+?>
+</body>
+</head>
+<html>
